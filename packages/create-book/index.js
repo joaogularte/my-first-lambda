655b643a71b2279429c280e3f0f9189e2ab9538a
joaogularte <jov96@hotmail.com>
2020-01-24 22:15:09 -0300

    Adicionado comentarios pertinentes

--- packages/create-book/index.js
+++ packages/create-book/index.js
@@ -1,14 +1,18 @@
 const { MongoClient } =  require('mongodb');
+
+//Variavel de ambiente que guarda a url do DB
 const MongoURI = process.env.MONGO_URL;
 
 exports.handler = async (event) => {
     const {name, description, author} = JSON.parse(event.body);
-
+    
+    //Validação dos atributos de entrada da função
     if(!name || !description || !author) return {
         statusCode: 400, 
         body: JSON.stringify({message: 'Insufficient attributes'})
     }
     
+    //Estabelecendo a conexão com o banco
     const connection = await MongoClient.connect(MongoURI, { useNewUrlParser: true, poolSize:10, useUnifiedTopology: true } )
     const db = connection.db('first-lambda');
     const colletion = db.collection('books');
